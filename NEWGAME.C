56eebc4f5c4e3b97ce0d701abd5ffdbf3324d473
Stevert <stevertlobo@gmail.com>
2018-04-05 00:02:15 +0530

    Add files via upload
    updated Car, moving roads and Input for arrow keys

--- NEWGAME.C
+++ NEWGAME.C
@@ -2,10 +2,18 @@
 #include<stdio.h>
 int main()
 {
+//initwindow(400,300);
 int gd,i,gm;
 gd=DETECT;
-initgraph(&gd,&gm,"c:\\turboc3\\bgi");
+initgraph(&gd,&gm,"C:\\TURBOC3\\BGI");
+int page=0,t_y=-300;
+int C_x=0,M_Speed=5;
+
+while(1){
 //background
+setactivepage(page);
+setvisualpage(1-page);
+cleardevice();
 setcolor(GREEN);
 rectangle(0,0,1000,1000);
 setfillstyle(1,GREEN);
@@ -15,13 +23,16 @@ setcolor(WHITE);
 rectangle(200,-1,400,500);
 setfillstyle(1,8);
 floodfill(201,0,WHITE);
-//track
-for(i=0;i<500;i+=65)
+//model for track
+for(i=0;i<400;i+=65)
 {
-rectangle(290,10+i,310,50+i);
+rectangle(290,10+i+t_y,310,50+i+t_y);
 setfillstyle(1,WHITE);
-floodfill(291,11+i,WHITE);
+floodfill(291,11+i+t_y,WHITE);
 }
+t_y+=15;
+if(t_y>500)
+    t_y=-500;
 
 //FOR LIFE AND POINTS
 settextstyle(11,0,2);
@@ -39,5 +50,39 @@ outtextxy(20,40,"POINTS:");
  setfillstyle(1,WHITE);
  floodfill(131,21,WHITE);
 
-getch();
+ outtextxy(100,40,"0");
+
+ //Car
+ setcolor(BLACK);
+ setfillstyle(1,RED);
+ rectangle(300+C_x,400,340+C_x,460);
+ floodfill(301+C_x,401,BLACK);
+
+ //Front
+ setfillstyle(5,YELLOW);
+ rectangle(305+C_x,385,335+C_x,400);
+ floodfill(306+C_x,386,BLACK);
+
+ //Hood
+
+ setfillstyle(9,CYAN);
+ rectangle(305+C_x,405,335+C_x,455);
+ floodfill(306+C_x,406,BLACK);
+
+ //Wheels
+ setfillstyle(1,BROWN);
+ pieslice(305+C_x,392,90,270,6);
+ pieslice(337+C_x,392,270,90,5);
+
+ if(GetAsyncKeyState(VK_LEFT))
+    C_x-=M_Speed;
+ else if(GetAsyncKeyState(VK_RIGHT))
+    C_x+=M_Speed;
+
+
+page=1-page;
+delay(20);
+
+
+}
 }
